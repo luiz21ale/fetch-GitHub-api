dbf0d75665b7dea31f30af89e7b93e1d2ac3e7bf
luiz <luizalexandre2143@gmail.com>
2024-11-20 00:45:39 -0300

    Projeto finalizado

---
+++ src/scripts/index.js
@@ -0,0 +1,47 @@
+import { getUser } from "./services/user.js"
+import { getRepositories } from "./services/repositories.js"
+
+import { user } from "./objects/user.js"
+import { screen } from "./objects/screen.js"
+
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    if(verificandoInputVazio(userName)) return
+    getUserData(userName)
+
+})
+
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const seEnterFoiPressionado = key === 13
+
+    if (seEnterFoiPressionado) {
+        if(verificandoInputVazio(userName)) return
+        getUserData(userName)
+    }
+})
+
+
+function verificandoInputVazio(userName){
+    if(userName.length === 0){
+        alert('Preencha o campo com o usuário do GitHub')
+        return true
+    }
+}
+
+async function getUserData(userName) {
+
+    
+    const userResponse = await getUser(userName)
+    const repositoriesResponse = await getRepositories(userName)
+    
+    if(userResponse.message === 'Not Found'){
+        screen.renderNotFound()
+        return
+    }
+    user.setInfo(userResponse)
+    user.setRepositories(repositoriesResponse)   
+    
+    screen.renderUser(user)
+}
---
+++ src/scripts/variables.js
@@ -0,0 +1,4 @@
+const baseUrl = 'https://api.github.com/users'
+const repositoriesQuantity = 7
+
+export {baseUrl, repositoriesQuantity}
